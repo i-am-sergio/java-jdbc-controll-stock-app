88de59162469e5adcb481389fb3008a51ce0cf08
sergio15dev <sergio15dev@gmail.com>
2023-04-08 15:21:58 -0500

    first commit

---
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package com.alura.jdbc.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+	private DataSource dataSource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("administrador");
+		pooledDataSource.setMaxPoolSize(10);
+		this.dataSource = pooledDataSource;
+	}
+	
+	public Connection recuperaConexion() throws SQLException {
+		return this.dataSource.getConnection();
+	}
+	
+}
---
+++ src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -0,0 +1,10 @@
+package com.alura.jdbc.modelo;
+
+public class Producto {
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private Integer cantidad;
+	
+	
+}
